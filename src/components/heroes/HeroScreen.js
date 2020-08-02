3eabe1492ed66041a3a73e3e971bcf4c1fa61620
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-01 22:03:32 -0500

    Additing useMemo in HeroList and HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useParams, Redirect } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = ({ history }) => {
 	const { heroeId } = useParams();
 
-	const hero = getHeroById(heroeId);
+	const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+
+	// const hero = getHeroById(heroeId);
 
 	if (!hero) {
 		return <Redirect to="/" />;
