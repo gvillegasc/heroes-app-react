76f6c93d5310bb74e767739b1ca7a766681f8d1d
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-08 16:09:49 -0500

    Protegiendo rutas

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { AppRouter } from './routes/AppRouter';
+import { AuthContext } from './auth/AuthContext';
+
+const init = () => {
+	return JSON.parse(localStorage.getItem('user')) || { logged: false };
+};
 
 export const HeroesApp = () => {
-	return <AppRouter />;
+	const [state, dispatch] = useReducer(reducer, {}, init);
+
+	return (
+		<AuthContext.Provider value={{}}>
+			<AppRouter />
+		</AuthContext.Provider>
+	);
 };
